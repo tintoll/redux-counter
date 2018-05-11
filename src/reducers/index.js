00828dd2f754166fcbd97a088d7554186d62ff40
blue <blue@tigrison.com>
2018-05-11 14:47:12 +0900

    리듀서를 여러개만들어서 합치는 작업을 해봄

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,44 @@
+import color from './color';
+import number from './number';
+
+import { combineReducers } from 'redux';
+
 /*
-  리듀서는 액션의 type에 따라 변화를 일으키는 함수입니다. 
-  이 리듀서 파일에는 최초변화를 일으키기전, 지니고 있어야할 초기상태가 정의되어야 한다. 
+    서브 리듀서들을 하나로 합칩니다.
+    combineReducers 를 실행하고 나면, 나중에 store의 형태가 파라미터로 전달한 객체의 모양대로 만들어집니다.
+    지금의 경우:
+    {
+        numberData: {
+            number: 0
+        },
+        colorData: {
+            color: 'black'
+        }
+    }
+    로 만들어집니다.
 */
+const reducers = combineReducers({
+  numberData : number,
+  colorData : color
+});
 
+export default reducers;
+
+
+/*
+// 리듀서는 액션의 type에 따라 변화를 일으키는 함수입니다. 
+// 이 리듀서 파일에는 최초변화를 일으키기전, 지니고 있어야할 초기상태가 정의되어야 한다. 
 import * as types from '../actions/ActionTypes';
-
 // 초기상태 정의 
 const initialState = {
   color : 'black',
   number : 0
 }
-/*
- 리듀서 함수는 state 와 action 을 파라미터로 가지는 함수이며
- 그 내부에서 switch문을 통하여 action.type에 따라 상태에 다른 변화를 일으켜주면 됩니다.
- 주의할점은, state를 직접 수정하면 절대 안되고, 
- 기존 state 값에 덮어쓴 새 상태객체를 만드는 방식으로 해야합니다.
-*/
 
+// 리듀서 함수는 state 와 action 을 파라미터로 가지는 함수이며
+// 그 내부에서 switch문을 통하여 action.type에 따라 상태에 다른 변화를 일으켜주면 됩니다.
+// 주의할점은, state를 직접 수정하면 절대 안되고, 
+// 기존 state 값에 덮어쓴 새 상태객체를 만드는 방식으로 해야합니다.
 function counter(state= initialState, action) {
   switch(action.type) {
     case types.INCREMENT:
@@ -40,3 +62,4 @@ function counter(state= initialState, action) {
 }
 
 export default counter;
+*/
